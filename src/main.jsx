14ae48a45aa6305388b55028aa762c7ac9cf396a
pradeek <pradeek.k@gmail.com>
2026-01-30 19:26:00 +0530

    004_11 completed

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,8 @@ import EditMovie from './components/EditMovie';
 import ManageCatalogue from './components/ManageCatalogue';
 import GraphQL from './components/GraphQL';
 import Login from './components/Login';
-import Movie from './components/Movie'
+import Movie from './components/Movie';
+import OneGenre from './components/OneGenre';
 
 const router = createBrowserRouter([
   {
@@ -32,6 +33,10 @@ const router = createBrowserRouter([
       {
         path: "/genres",
         element: <Genres/>
+      },
+      {
+        path: "/genres/:id",
+        element: <OneGenre />
       },
       {
         path: "/admin/movie/0",
