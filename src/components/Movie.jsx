e27a1f762ea7df0389cf49fff9d75fbae4c4151d
pradeek <pradeek.k@gmail.com>
2026-01-30 13:26:41 +0530

    029_10 completed

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -30,7 +30,7 @@ const Movie = () => {
         movie.genres = [];
     }
 
-    return(
+    return (
         <div>
             <h2>Movie: {movie.title}</h2>
             <small><em>{movie.release_date}, {movie.runtime} minutes, Rated {movie.mpaa_rating}</em></small><br />
